8d715056a4d9c129e031ed2c406907f80bb1f677
Ravi Kiran Goud Jaggari <122633050+Ravikiran-goud@users.noreply.github.com>
2024-04-26 01:12:40 -0400

    Enemy Player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
-    public GameObject focalPoint;
+    private GameObject focalPoint;
     public float speed = 5.0f;
     
     // Start is called before the first frame update
@@ -20,6 +20,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float forwardInput = Input.GetAxis("Vertical");
-        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+        playerRb.AddForce(Vector3.forward * speed * forwardInput);
+        // playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
     }
 }
